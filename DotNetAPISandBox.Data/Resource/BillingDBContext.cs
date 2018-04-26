588bd227b7beb031bd623a841b6d3729e7891799
James Walsh Jr <walshjrjames@outlook.com>
2018-04-26 16:37:48 -0400

    Initial check in of DAL components Entity Framework in Data layer Entity Framework in Service layer

---
+++ DotNetAPISandBox.Data/Resource/BillingDBContext.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DotNetAPISandBox.Data.Resource
+{
+    public class BillingContext : DbContext
+    {
+        public const string ConnectionString = "name=Billing";
+        public BillingContext() : this(ConnectionString)
+        {
+
+        }
+
+        private BillingContext(string nameOrConnectionString)
+            : base(nameOrConnectionString)
+        {
+            Database.SetInitializer<BillingContext>(null);
+            Configuration.ValidateOnSaveEnabled = false;
+        }
+
+        protected override void OnModelCreating(DbModelBuilder modelBuilder)
+        {
+            base.OnModelCreating(modelBuilder);
+        }
+    }
+}
